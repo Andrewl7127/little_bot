857a6577d86c68687c7b562dbe7aa3d15dd3727d
Jessie Tsai <jessiets@usc.edu>
2023-01-28 01:30:32 -0800

    added randomizing agents function

--- randomize.py
+++ randomize.py
@@ -3,3 +3,40 @@ async def randomMap(message):
 
 async def randomAgent(message):
     return
+
+async def randomAgent(message):
+    number = message.content.split(' ')
+    if len(number) > 1: 
+        if int(number[1]) > 5:
+            await message.channel.send(f'{message.author.mention} HEY! THATS TOO MANY PPL')
+            return
+        number = int(number[1])
+    else:
+        number = 1
+
+    controllers = ['Astra', 'Brimstone', 'Omen', 'Viper', 'Harbor']
+    duelists = ['Jett', 'Neon', 'Phoenix', 'Raze', 'Reyna', 'Yoru']
+    sentinels = ['Chamber', 'Cypher', 'Killjoy', 'Sage']
+    initiators = ['Breach', 'KAY/O', 'Skye', 'Sova']
+
+    roles = [controllers, duelists, sentinels, initiators]
+    random.shuffle(roles)
+    agent = []
+    for n in range(number):
+        if n < 4:
+            agent_index = random.randint(0, len(roles[n]) - 1)
+            agent.append(roles[n][agent_index])
+        else:
+            role_index = random.randint(0, len(roles) - 1)
+            agent_index = random.randint(0, len(roles[role_index]) - 1)
+            if roles[role_index][agent_index] in agent:
+                agent_index -= 1
+            agent.append(roles[role_index][agent_index])
+    
+    if number == 1:
+        await message.channel.send(f'{message.author.mention} ' + agent[0])
+    else:
+        msg = ''
+        for n in range(number):
+            msg += 'Player ' + str(n+1) + ': ' + agent[n] + '\n'
+        await message.channel.send(f'{message.author.mention} \n' + msg)
